Fixed ratio normalisation bug in CombineNetCDF, and changed examples in CombineNetCDF and NcGen.py.

CombineNetCDF.py
@@ -5,9 +5,10 @@ import netCDF4 as nc
 def CombineCDFs(output,files,ratios,absdir):
     
     ratios=np.array(ratios)
-    norm=np.dot(ratios,ratios)**0.5
+    norm=np.sum(ratios)
+    print(ratios)
     ratios=ratios/norm
-
+    print(ratios,norm,np.sum(ratios))
     for ratio in ratios:
         if ratio < 0:
             print('One of the fractions is negative!')
@@ -34,6 +35,8 @@ def CombineCDFs(output,files,ratios,absdir):
     t_calc[:]=file_['t_calc'][:]
     p_calc[:]=file_['p_calc'][:]
     nu_var.step= abs(file_['nu'][1]-file_['nu'][0])
+    print('Creating {0}!'.format(output))
+
     print(nu_var.step)
     file_.close()
 
@@ -45,7 +48,7 @@ def CombineCDFs(output,files,ratios,absdir):
     # Iterates over all PT points and weights them according 
     # to the supplied ratios
     for i in range(0,pt_num):
-        print(i,pt_num)
+        print(i,pt_num,end='\r',flush=True)
         file_=filesNC[0]
         kabs_weighted=np.array(file_['kabs'][i,:])*ratios[0]
         for j in range(1,len(files)):
@@ -57,28 +60,42 @@ def CombineCDFs(output,files,ratios,absdir):
         file_.close()
     del filesNC
     fnc.close()
-    print(output,'has been created!')
+    print('\n',output,'has been created!')
     return None
 
 ####### End of functions
 ####### Beginning of work area
 
 
+HAbundances={ '1': 0.999885, '2': 0.000115}
+SAbundances={ '32': 0.9499, '33':0.0075, '34': 0.0425,'36':0.0001}
+SiAbundances={'28': 0.92223,'29':0.04685,'30':0.03092}
+NaAbundances={'23': 1.0}
+AlAbundances={'27': 1.0}
+OAbundances={'16': 0.99757, '17': 0.00038,'18': 0.00205}
+CaAbundances={'40': 0.96941}
+TiAbundances={'46': 0.0825, '47': 0.0744, '48': 0.7372, '49': 0.0541, '50': 0.0518}
+MgAbundances={'24': 0.7899, '25': 0.1000, '26':0.1101}
+
+
+# NaH
+ratios_NaH=[]
+HCount=[1,2]
+NaCount=[23,23]
+files_NaH=[]
+for i in range(len(HCount)):
+    Na=NaCount[i]
+    H=HCount[i]
+    abund=NaAbundances[str(Na)]*HAbundances[str(H)]
+    ratios_NaH.append(abund)
+    filename='abs_coeff_{0}Na-{1}H_Rivlin_pt800.nc'.format(Na,H)
+    files_NaH.append(filename)
 
 absdir='/home/dc-ridg1/AbsCoeffs/'
-files=[]
-for i in [46,47,48,49,50]:
-    filename='abs_coeff_{0}Ti-16O_Toto_pt800.nc'.format(i)
-    files.append(filename)
+filename_NaH='abs_coeff_NaH_Rivlin_TerrestrialAbund_pt800.nc'
+CombineCDFs(filename_NaH,files_NaH,ratios_NaH,absdir)
 
 # Source for abundances: 
 # https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
-ratios=[0.0825,
-        0.0744,
-        0.7372,
-        0.0541,
-        0.0518]
 
-filename='abs_coeff_TiO_Toto_SolarMetallicity_pt800.nc'
 
-CombineCDFs(filename,files,ratios,absdir)

JobSubmit.py
@@ -25,6 +25,22 @@ def PTGen(numP,numT,Ps,Ts):
             PTPairs[1].append(temp)
     return PT_idx,PTPairs
 
+def ptRead(filename):
+    f=open(filename,'r')
+    lines=f.readlines()
+    f.close()
+    count=0
+    pressure=[]
+    temperature=[]
+    for line in lines:
+        line=line.rstrip()
+        line=line.split(' ')
+        for i in range(0,len(line)):
+            p=float(line[0].replace("D","E"))
+            
+            pressure.append(np.float64(line[0]))
+            temperature.append(np.float64(line[i]))
+
 # Submits jobs (assuming DiRaC)
 def QSubmit(input_file,species,idx):
     line=['qsub submit_job.pbs -v param_file={0} -N AbsCoeff_{1}_{2}'.format(input_file,species,idx)]
@@ -49,11 +65,14 @@ def JobSubmit(species,PTPairs,range_,maxJobs=100,forceRecreation=False,note=''):
         pressure= PTPairs[0][i]
         temperature = PTPairs[1][i]
     
-        filenameInp='{0}_{1}_{2:6.3e}_{3:6.3e}_{4}_{5}{6}.inp'.format(species.molecule,species.source,temperature,pressure,range_[0],range_[1],note)
-        filenameXsec='{0}_{1}_{2:6.3e}_{3:6.3e}_{4}_{5}{6}.xsec'.format(species.molecule,species.source,temperature,pressure,range_[0],range_[1],note)
+        filenameInp='{0}_{1}_{2:8.7e}_{3:8.7e}_{4}_{5}{6}.inp'.format(species.molecule,species.source,temperature,pressure,range_[0],range_[1],note)
+        filenameXsec='{0}_{1}_{2:8.7e}_{3:8.7e}_{4}_{5}{6}.xsec'.format(species.molecule,species.source,temperature,pressure,range_[0],range_[1],note)
         numJobs=JobCount()
-        # Only submits job if file 
-        if not os.path.isfile(filenameXsec) or forceRecreation:
+        # Only submits job if file doesn't exist or recreation asked
+        # Just for TiO
+        xsecfolder=os.getcwd()+'/{0}/'.format(species.molecule)
+        if not os.path.isfile(xsecfolder+filenameXsec) or forceRecreation:
+            #print(filenameInp,species.molecule,i)
             QSubmit(filenameInp,species.molecule,i)
             time.sleep(5)
             numJobs=JobCount()
@@ -70,88 +89,115 @@ def JobSubmit(species,PTPairs,range_,maxJobs=100,forceRecreation=False,note=''):
             if numJobs < maxJobs:
                 print('')
 
-    print('All jobs have been submitted!')
+    print('All jobs have been submitted for {0}!'.format(species.molecule))
     return None
 
-#
-#
-#
+
 ###### End of Functions, work area is below
-#
-#
-#
 
 
 
 # This assume we're looking at a Hot Jupiter
 H2=InpW.Peturber()
-H2.UpdateVars(species='H2',gamma=0.0207,n=0.44,t0=298.0,
-               file='NewSpecies/LineWidths/H2O/1H2-16O__H2.broad', 
-              model='JJ',ratio=0.8379481147247214)
+H2.UpdateVars(species='H2',gamma=0.05,n=0.6,t0=296.0,p0=1.0, ratio=0.8379481147247214)
 H2.StringCreate()
 
 He=InpW.Peturber()
-He.UpdateVars(species='He',gamma=0.043,n=0.02,t0=298.0,
-              file='NewSpecies/LineWidths/H2O/1H2-16O__He.broad', 
-              model='JJ',ratio=0.16205188527527858)
+He.UpdateVars(species='He',gamma=0.05,n=0.4,t0=296.0,p0=1.0, ratio=0.16205188527527858)
 He.StringCreate()
 
 
-## Air is assuming nitrogen dominated atmosphere
+## Air is assuming nitrogen dominated
 air=InpW.Peturber()
-air.UpdateVars(species='air',t0=298.0, file='NewSpecies/LineWidths/H2O/1H2-16O__air_a0.broad',
-               model='J', ratio = 1.0)
+air.UpdateVars(species='air',t0=298.0, file='NewSpecies/LineWidths/H2O/1H2-16O__air_a0.broad',model='J', ratio = 1.0)
 air.StringCreate()
 
 
-H2O=InpW.Species()
-folder='NewSpecies/LineLists/H2O/POKAZATEL/'
-H2O.UpdateVars(mass=18.02,molecule='H2O',source='POKAZATEL',states='NewSpecies/LineLists/H2O/POKAZATEL/1H2-16O__POKAZATEL.states', pffile ='NewSpecies/PartitionFunctions/H2O/1H2-16O__POKAZATEL.pf')
-H2O.UpdateTransitions(folder)
+
+
+TiO_46_16=InpW.Species()
+folder='NewSpecies/LineLists/TiO/Toto/'
+TiO_46_16.UpdateVars(mass=61.947543,molecule='46Ti-16O',source='Toto',states='NewSpecies/LineLists/TiO/Toto/46Ti-16O__Toto.states', pffile ='NewSpecies/PartitionFunctions/TiO/46Ti-16O__Toto.pf')
+TiO_46_16.UpdateTransitions(folder)
+
+TiO_47_16=InpW.Species()
+folder='NewSpecies/LineLists/TiO/Toto/'
+TiO_47_16.UpdateVars(mass=62.946663,molecule='47Ti-16O',source='Toto',states='NewSpecies/LineLists/TiO/Toto/47Ti-16O__Toto.states', pffile ='NewSpecies/PartitionFunctions/TiO/47Ti-16O__Toto.pf')
+TiO_47_16.UpdateTransitions(folder)
+
+TiO_48_16=InpW.Species()
+folder='NewSpecies/LineLists/TiO/Toto/'
+TiO_48_16.UpdateVars(mass=63.942857,molecule='48Ti-16O',source='Toto',states='NewSpecies/LineLists/TiO/Toto/48Ti-16O__Toto.states', pffile ='NewSpecies/PartitionFunctions/TiO/48Ti-16O__Toto.pf')
+TiO_48_16.UpdateTransitions(folder)
+
+TiO_49_16=InpW.Species()
+folder='NewSpecies/LineLists/TiO/Toto/'
+TiO_49_16.UpdateVars(mass=64.942780,molecule='49Ti-16O',source='Toto',states='NewSpecies/LineLists/TiO/Toto/49Ti-16O__Toto.states', pffile ='NewSpecies/PartitionFunctions/TiO/49Ti-16O__Toto.pf')
+TiO_49_16.UpdateTransitions(folder)
+
+TiO_50_16=InpW.Species()
+folder='NewSpecies/LineLists/TiO/Toto/'
+TiO_50_16.UpdateVars(mass=65.939702,molecule='50Ti-16O',source='Toto',states='NewSpecies/LineLists/TiO/Toto/50Ti-16O__Toto.states', pffile ='NewSpecies/PartitionFunctions/TiO/50Ti-16O__Toto.pf')
+TiO_50_16.UpdateTransitions(folder)
 
 
 # Range (cm-1)
-range_=[0,41200]
+
+offset=0.05
+range_=[0.0005,29999.9995]
+npoints=30000001 # 0.1 m-1 spacing
+
+wavenumber=np.linspace(range_[0],range_[1],num=npoints)
+print('The spacing is ',np.diff(wavenumber)[0]*100, 'm-1, is this correct?',flush=True)
+import time
+time.sleep(5)
+broadeners=[H2.String,He.String]
+note='_HotJupiter'
 
 # Number of Pressure, Temperature points
 num_P=40
-num_T=40
+num_T=20
 
 # Pressure Limits (Bar)
-Pmin=1e-6
+Pmin=1e-11
 Pmax=1e3
+
+# 1 Bar = 10^5 Pa
+# 1 Pa = 10^-5 Bar
+# Matches Davids PT800 file
+Pmin = 7.8804628e-05*1e-5 
+Pmax = 1e3 # 10^8 Pa
+
 
 # Temperature Limits (K)
 Tmin=70
 Tmax=6000
 
 
-
-# 1 Bar = 10^5 Pa
-# 1 Pa = 10^-5 Bar
 # Matches Davids PT800 file
-
-num_P=40
-num_T=20
-range_=[0.0005,41199.9995]
-Pmin = 7.8804628e-05*1e-5 
-Pmax = 1e3 # 10^8 Pa
 Tmin=70
-Tmax=3000
+Tmax=3000 
+
 P=[Pmin,Pmax]
 T=[Tmin,Tmax]
 
+offset=1e-06
 PT_idx,PTPairs=PTGen(num_P,num_T,P,T)
 
 
-range_=[0.0005,41199.9995]
-npoints=41200001 # 0.1 m-1 spacing
-broadeners=[H2.String,He.String]
-note='_HotJupiterHiRez'
-InpCreate(H2O, PT_idx,PTPairs,npoints,range_,broadeners,note=note)
+InpCreate(TiO_46_16, PT_idx,PTPairs,npoints,range_,broadeners,offset=offset,note=note)
+InpCreate(TiO_47_16, PT_idx,PTPairs,npoints,range_,broadeners,offset=offset,note=note)
+InpCreate(TiO_48_16, PT_idx,PTPairs,npoints,range_,broadeners,offset=offset,note=note)
+InpCreate(TiO_49_16, PT_idx,PTPairs,npoints,range_,broadeners,offset=offset,note=note)
+InpCreate(TiO_50_16, PT_idx,PTPairs,npoints,range_,broadeners,offset=offset,note=note)
 
 # Loop for submitting jobs
 
-JobSubmit(H2O,PTPairs,range_,note=note)
+forceRec=False
+JobSubmit(TiO_46_16,PTPairs,range_,maxJobs=300,forceRecreation=forceRec,note=note)
+JobSubmit(TiO_47_16,PTPairs,range_,maxJobs=300,forceRecreation=forceRec,note=note)
+JobSubmit(TiO_48_16,PTPairs,range_,maxJobs=300,forceRecreation=forceRec,note=note)
+JobSubmit(TiO_49_16,PTPairs,range_,maxJobs=300,forceRecreation=forceRec,note=note)
+JobSubmit(TiO_50_16,PTPairs,range_,maxJobs=300,forceRecreation=forceRec,note=note)
 
 

NcGen.py
@@ -142,21 +142,26 @@ def NcCreate(filename,mass,homedir,absdir,datadir,xsecfolder,npyfolder,npoints_P
 import shutil
 homedir='/home/dc-ridg1/data/Exocross/'
 absdir='/home/dc-ridg1/AbsCoeffs/'
-datadir='/data/dp015/dc-ridg1/NewSpecies/AbsCoeffs/TiO/'
+datadir='/data/dp015/dc-ridg1/NewSpecies/AbsCoeffs/AlO/'
 
+
+AlCount=[26,27,27,27]
+OCount=[16,16,17,18]
 
 # https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
-Omass=15.99491461956
-masses= [45.9526316,
-         46.9517631,
-         47.9479463,
-         48.9478700,
-         49.9447912]
-masses=(np.array(masses)+Omass)*1.6726e-27
+# in Da
+masses=[41.981807, # 26Al-16O
+        42.976454, # 27Al-16O
+        43.980671, # 27Al-17O
+        44.980700] # 27Al-18O
+Da_kg=1.660539e-27
+masses=(np.array(masses))*Da_kg
 for i in range(0,len(masses)):
-    xsecfolder=homedir+'Results/TiO_Toto/{0}Ti-16O/'.format(i+46)
-    npyfolder=homedir+'npys/TiO_Toto/{0}Ti-16O/'.format(i+46)
-    filename='abs_coeff_{0}Ti-16O_Toto_pt800.nc'.format(i+46)
+    Al=AlCount[i]
+    O=OCount[i]
+    xsecfolder=homedir+'Results/AlO_ATP/{0}Al-{1}O/'.format(Al,O)
+    npyfolder=homedir+'npys/AlO_ATP/{0}Al-{1}O/'.format(Al,O)
+    filename='abs_coeff_{0}Al-{1}O_ATP_pt800.nc'.format(Al,O)
     NcCreate(filename,masses[i],homedir,absdir,datadir,xsecfolder,npyfolder,npoints_P=40,npoints_T=20)
     shutil.copy(absdir+filename,datadir+filename)
 